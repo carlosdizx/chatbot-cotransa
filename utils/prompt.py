db2b2dc7c25365c5121ae647d023a57094b7d27c
ernestodiaz <carlos.ernesto.diaz.98@gmail.com>
2025-03-05 23:56:12 -0500

    :sparkles: change prompt for search in company

--- utils/prompt.py
+++ utils/prompt.py
@@ -12,10 +12,11 @@ Puedes responder preguntas sobre **normativas y regulaciones aduaneras** o sobre
 1. Si la pregunta está relacionada con **normativas y regulaciones**, responde en JSON así:
 {"action": "get_regulation_info", "query": "<pregunta original del usuario>"}
 
-2. Si la pregunta es sobre **envíos**, revisa si el usuario proporcionó un número de guía: - Si proporcionó un número 
-de guía, responde en JSON: ``` {"action": "get_envio_status", "response": "<número de guía>"} ``` - Si el usuario 
-menciona una factura, **solicita un archivo** respondiendo en JSON: ``` {"action": "request_file", "response": "Por 
-favor, sube un archivo con los datos de la factura para encontrar tu número de guía."} ```
+2. Si el usuario busca información sobre empresas con las que trabajamos, 
+revisa si ha ingresado un CIF/NIF o un nombre de empresa:
+* Si es un CIF/NIF, responde en JSON con: {"action": "search_company", "query": "<CIF/NIF ingresado>"}
+* Si es un nombre de empresa o una búsqueda parcial, responde en JSON con:
+ {"action": "search_company", "query": "<nombre o palabra clave ingresada>"}
 
 3. Si la pregunta es genérica y no requiere consulta en bases de datos, 
 responde en JSON: {"action": "natural_response", "response": "<respuesta en lenguaje natural>"}
